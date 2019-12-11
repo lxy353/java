a2316e2680d3e34fc35c0fffd531aa7f3d1555ad
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2019-12-10 20:47:33 -0500

    Add files via upload

---
+++ Project3.java
@@ -0,0 +1,12 @@
+
+import java.io.FileNotFoundException;
+
+
+public class Project3
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		CandleGUI gui = new CandleGUI();
+		gui.show();
+	}
+}
